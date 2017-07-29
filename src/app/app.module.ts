f74b28bba50ebe4e9c6cf082b0c11adb4acec664
James Le <le_j6@denison.edu>
2017-07-29 15:05:50 -0700

    Install infinite-scrollable gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { Picture } from '../components/picture/picture';
 import { PictureUpload } from '../components/picture_upload/picture_upload';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
     PictureUpload
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule
   ],
   providers: [CosmicConfigService],
   bootstrap: [AppComponent]
