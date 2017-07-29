b2e11ed15b60514aa661a309d447257784ac4a7e
James Le <le_j6@denison.edu>
2017-07-29 15:02:08 -0700

    Concatenating everything together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Cosmic from 'cosmicjs';
+import {CosmicConfigService} from '../services/cosmic_config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public items = [];
+  page = 0;
+  page_size = 2;
+  scrollEnabled = true;
+
+  constructor(
+    public cosmicCfg: CosmicConfigService
+  ) {
+    this.reload();
+  }
+
+  reload() {
+    this.items = [];
+    this.page = 0;
+    this.scrollEnabled = true;
+    let params = {
+      type_slug: this.cosmicCfg.getPhotoSlug(),
+      limit: this.page_size,
+      skip: 0
+    };
+    Cosmic.getObjectType(this.cosmicCfg.getReadCfg(), params, (err, res) => {
+      this.items = res.objects.all;
+    });
+  }
+
+  onUpload() {
+    this.reload();
+  }
+
+  onScroll() {
+    if (!this.scrollEnabled) {
+      return;
+    }
+    this.page++;
+    let params = {
+      type_slug: this.cosmicCfg.getPhotoSlug(),
+      limit: this.page_size,
+      skip: this.page * this.page_size
+    };
+    Cosmic.getObjectType(this.cosmicCfg.getReadCfg(), params, (err, res) => {
+      if (res.objects && res.objects.all) {
+        res.objects.all.forEach((itm) => {
+          this.items.push(itm);
+        });
+      }
+      else {
+        this.scrollEnabled = false;
+      }
+    });
+  }
 }
