2c0e7aa5f7e2d344d073fb70718e09dc0101a548
James Le <le_j6@denison.edu>
2017-07-29 15:19:46 -0700

    Quick Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  write_key: 'SpZZix3ww8IZZLRXc22MQCtaUwNzgiKvbw4N1W5fz94V0pGobt',
+  bucket_name: 'cosmic-angular',
+  photos_type: 'photos'
 };
